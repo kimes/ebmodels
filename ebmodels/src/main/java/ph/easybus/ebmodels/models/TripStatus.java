b5d91fe7845a7f51169642d409e54e440d6a3eff
Kimuel Santos <kimdub28@gmail.com>
2023-09-14 10:38:49 +0800

    add custom status

--- ebmodels/src/main/java/ph/easybus/ebmodels/models/TripStatus.java
+++ ebmodels/src/main/java/ph/easybus/ebmodels/models/TripStatus.java
@@ -21,7 +21,8 @@ public class TripStatus extends BaseObservable implements Parcelable {
     @Bindable
     private int status = 0;
 
-    private String tripId = "", lane = "", terminal = "";
+    @Bindable
+    private String tripId = "", lane = "", terminal = "", customStatus = "";
 
     @Bindable
     private Date tripDate = Calendar.getInstance().getTime(), dispatchedDate = null;
@@ -33,6 +34,7 @@ public class TripStatus extends BaseObservable implements Parcelable {
             if (object.has("trip_id")) tripId = object.getString("trip_id");
             if (object.has("lane")) lane = object.getString("lane");
             if (object.has("terminal")) terminal = object.getString("terminal");
+            if (object.has("custom_status")) customStatus = object.getString("custom_status");
 
             if (object.has("trip_date")) {
                 tripDate = DateTimeUtils.toDate(object.getString("trip_date"));
@@ -53,11 +55,12 @@ public class TripStatus extends BaseObservable implements Parcelable {
         parcel.readIntArray(ints);
         status = ints[0];
 
-        String[] strings = new String[3];
+        String[] strings = new String[4];
         parcel.readStringArray(strings);
         tripId = strings[0];
         lane = strings[1];
         terminal = strings[2];
+        customStatus = strings[3];
 
         long[] dates = new long[2];
         parcel.readLongArray(dates);
@@ -73,6 +76,8 @@ public class TripStatus extends BaseObservable implements Parcelable {
             object.put("lane", lane);
             object.put("terminal", terminal);
             object.put("trip_date", DateTimeUtils.toISODate(tripDate));
+            object.put("custom_status", customStatus);
+
             if (dispatchedDate != null) {
                 object.put("dispatched_date", DateTimeUtils.toISODateUtc(dispatchedDate));
             }
@@ -84,7 +89,7 @@ public class TripStatus extends BaseObservable implements Parcelable {
 
     public void writeToParcel(Parcel parcel, int flags) {
         parcel.writeIntArray(new int[] { status });
-        parcel.writeStringArray(new String[] { tripId, lane, terminal });
+        parcel.writeStringArray(new String[] { tripId, lane, terminal, customStatus });
         parcel.writeLongArray(new long[] { tripDate.getTime(),
                 (dispatchedDate != null) ? dispatchedDate.getTime() : -1 });
     }
@@ -95,6 +100,7 @@ public class TripStatus extends BaseObservable implements Parcelable {
     public String getTripId() { return tripId; }
     public String getLane() { return lane; }
     public String getTerminal() { return terminal; }
+    public String getCustomStatus() { return customStatus; }
     public Date getTripDate() { return tripDate; }
     public Date getDispatchedDate() { return dispatchedDate; }
 
@@ -105,6 +111,10 @@ public class TripStatus extends BaseObservable implements Parcelable {
     public void setTripId(String tripId) { this.tripId = tripId; }
     public void setLane(String lane) { this.lane = lane; }
     public void setTerminal(String terminal) { this.terminal = terminal; }
+    public void setCustomStatus(String customStatus) {
+        this.customStatus = customStatus;
+        notifyPropertyChanged(BR.customStatus);
+    }
     public void setTripDate(Date tripDate) { this.tripDate = tripDate; }
     public void setDispatchedDate(Date dispatchedDate) {
         this.dispatchedDate = dispatchedDate;
