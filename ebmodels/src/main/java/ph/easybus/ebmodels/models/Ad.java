eab8920cf9049d100952d8425dc32de0da8cd407
Kimuel Santos <kimdub28@gmail.com>
2024-11-12 14:54:30 +0800

    add appear per cycler

--- ebmodels/src/main/java/ph/easybus/ebmodels/models/Ad.java
+++ ebmodels/src/main/java/ph/easybus/ebmodels/models/Ad.java
@@ -14,14 +14,25 @@ import org.json.JSONObject;
 public class Ad extends BaseObservable implements Parcelable {
 
     @Bindable
+    private int appearPerCycle = 1, appearanceCount = 0;
+
+    @Bindable
     private String mongoId = "", video = "";
 
-    public Ad(String mongoId, String video) {
+    public Ad() {}
+
+    public Ad(String mongoId, String video, int appearPerCycle) {
         this.mongoId = mongoId;
         this.video = video;
+        this.appearPerCycle = appearPerCycle;
     }
 
     public Ad(Parcel parcel) {
+        int[] ints = new int[2];
+        parcel.readIntArray(ints);
+        appearPerCycle = ints[0];
+        appearanceCount = ints[1];
+
         String[] strings = new String[2];
         parcel.readStringArray(strings);
         mongoId = strings[0];
@@ -32,6 +43,7 @@ public class Ad extends BaseObservable implements Parcelable {
         try {
             this.mongoId = object.getString("_id");
             this.video = object.getString("video");
+            this.appearPerCycle = object.getInt("appear_per_cycle");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -42,6 +54,7 @@ public class Ad extends BaseObservable implements Parcelable {
         try {
             object.put("_id", mongoId);
             object.put("video", video);
+            object.put("appear_per_cycle", appearPerCycle);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -50,14 +63,28 @@ public class Ad extends BaseObservable implements Parcelable {
 
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeIntArray(new int[] { appearPerCycle, appearanceCount });
         dest.writeStringArray(new String[] { mongoId, video });
     }
 
     @Override
     public int describeContents() { return 0; }
 
+    public int getAppearPerCycle() { return appearPerCycle; }
+    public int getAppearanceCount() { return appearanceCount; }
+
     public String getMongoId() { return mongoId; }
     public String getVideo() { return video; }
+
+    public void setAppearPerCycle(int appearPerCycle) {
+        this.appearPerCycle = appearPerCycle;
+        notifyPropertyChanged(BR.appearPerCycle);
+    }
+
+    public void setAppearanceCount(int appearanceCount) {
+        this.appearanceCount = appearanceCount;
+        notifyPropertyChanged(BR.appearanceCount);
+    }
 
     public void setMongoId(String mongoId) {
         this.mongoId = mongoId;
