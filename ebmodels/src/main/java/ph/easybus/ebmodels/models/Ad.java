e605544c400f8c8c594d1b335dbd92f1f9e37db8
Kimuel Santos <kimdub28@gmail.com>
2025-07-01 10:00:07 +0800

    add ad start date, expiry date

--- ebmodels/src/main/java/ph/easybus/ebmodels/models/Ad.java
+++ ebmodels/src/main/java/ph/easybus/ebmodels/models/Ad.java
@@ -11,6 +11,11 @@ import androidx.databinding.library.baseAdapters.BR;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import ph.easybus.ebmodels.utils.DateTimeUtils;
+
 public class Ad extends BaseObservable implements Parcelable {
 
     @Bindable
@@ -18,6 +23,9 @@ public class Ad extends BaseObservable implements Parcelable {
 
     @Bindable
     private String mongoId = "", video = "";
+
+    private Date startDate = Calendar.getInstance().getTime(),
+            expiryDate = Calendar.getInstance().getTime();
 
     public Ad() {}
 
@@ -33,6 +41,11 @@ public class Ad extends BaseObservable implements Parcelable {
         appearPerCycle = ints[0];
         appearanceCount = ints[1];
 
+        long[] dates = new long[2];
+        parcel.readLongArray(dates);
+        startDate = new Date(dates[0]);
+        expiryDate = new Date(dates[1]);
+
         String[] strings = new String[2];
         parcel.readStringArray(strings);
         mongoId = strings[0];
@@ -44,6 +57,8 @@ public class Ad extends BaseObservable implements Parcelable {
             this.mongoId = object.getString("_id");
             this.video = object.getString("video");
             this.appearPerCycle = object.getInt("appear_per_cycle");
+            this.startDate = DateTimeUtils.toDateUtc("start_date");
+            this.expiryDate = DateTimeUtils.toDateUtc("expiry_date");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -55,6 +70,8 @@ public class Ad extends BaseObservable implements Parcelable {
             object.put("_id", mongoId);
             object.put("video", video);
             object.put("appear_per_cycle", appearPerCycle);
+            object.put("start_date", DateTimeUtils.toISODateUtc(startDate));
+            object.put("expiry_date", DateTimeUtils.toISODateUtc(expiryDate));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -64,6 +81,9 @@ public class Ad extends BaseObservable implements Parcelable {
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
         dest.writeIntArray(new int[] { appearPerCycle, appearanceCount });
+        dest.writeLongArray(new long[] {
+                startDate.getTime(), expiryDate.getTime()
+        });
         dest.writeStringArray(new String[] { mongoId, video });
     }
 
@@ -75,6 +95,8 @@ public class Ad extends BaseObservable implements Parcelable {
 
     public String getMongoId() { return mongoId; }
     public String getVideo() { return video; }
+    public Date getStartDate() { return startDate; }
+    public Date getExpiryDate() { return expiryDate; }
 
     public void setAppearPerCycle(int appearPerCycle) {
         this.appearPerCycle = appearPerCycle;
@@ -94,6 +116,14 @@ public class Ad extends BaseObservable implements Parcelable {
     public void setVideo(String video) {
         this.video = video;
         notifyPropertyChanged(BR.video);
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
     }
 
     public static Parcelable.Creator<Ad> CREATOR = new Creator<Ad>() {
